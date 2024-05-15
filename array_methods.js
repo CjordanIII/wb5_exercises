0048c4b9695458ea3a8175d011a640ceda31e075
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-15 15:25:33 -0500

    search

---
+++ array_methods.js
@@ -0,0 +1,24 @@
+let cart = [
+  { item: "Bread", price: 3.29, quantity: 2 },
+  { item: "Milk", price: 4.09, quantity: 1 },
+  { item: "T-Bone Steak", price: 12.99, quantity: 2 },
+  { item: "Baking Potato", price: 1.89, quantity: 6 },
+  { item: "Iceberg Lettuce", price: 2.06, quantity: 1 },
+  { item: "Ice Cream - Vanilla", price: 6.81, quantity: 1 },
+  { item: "Apples", price: 0.66, quantity: 6 },
+];
+
+const itemNames = cart.map((cartItem) => {
+  return cartItem.item;
+});
+// sorted
+itemNames.sort().forEach((cartItem) => {
+  console.log("items in the cart=>>>", cartItem);
+});
+// reduce
+const totalItems = cart.reduce((acc, item) => {
+  acc += item.quantity * item.price;
+  return acc;
+}, 0);
+
+console.log("your total===>", totalItems);
